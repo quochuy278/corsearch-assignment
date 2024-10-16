08ad156758ed1b906f91c8e62aba5f5928e7aa00
quochuy278 <quochuy270800@gmail.com>
2024-10-16 20:39:11 +0300

    refactor: change style file name

--- src/pages/UserDashboardPage/index.tsx
+++ src/pages/UserDashboardPage/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
+import React from "react";
 // import { UserList } from '../../features/userDashboard/components';
-import styles from './UserDashboardPage.module.scss';
+import "./styles.scss";
 
 const UserDashboardPage: React.FC = () => {
   return (
-    <div className={styles.userDashboardPage}>
+    <div className="">
       <h1>User Dashboard</h1>
       {/* <UserList /> */}
       {/* Add other components from the userDashboard feature as needed */}
