d325a5620dd8e244e6770e21a8a9d53e93a517f9
quochuy278 <quochuy270800@gmail.com>
2024-10-16 22:18:56 +0300

    wrap app with react query provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,12 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routers/index.tsx";
 import "./shared/styles/index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
-  <RouterProvider router={routes} />
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={routes} />
+  </QueryClientProvider>
 );
